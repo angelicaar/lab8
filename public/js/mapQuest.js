231774090f31a3d9c64f96bcd3f24345fa7e82ee
Angelica Ramos <amr052@ucsd.edu>
2019-02-28 10:43:00 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
-function initMap() {
-	// add your code here
+function initMap(){
+	L.mapquest.key = 'XPOPblN3UezAD1fghnu8BHDtYvXtGGkc';
+
+	var map = L.mapquest.map("map",{
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer("map"),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map); 
+
 }
